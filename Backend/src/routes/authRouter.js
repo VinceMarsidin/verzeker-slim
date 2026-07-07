5908295177ce3679d42aea99506e79b62a787656
Vince <v.bahri@unasat.sr>
2026-07-07 16:21:57 -0300

    remove old project

--- Backend/src/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as authController from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/login', authController.login);
-
-export default router;
