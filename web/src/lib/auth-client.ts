53c2b50ed106398ff543b0f8f680e0636a6c9682
dwayneR06 <riedewalddwayne12@gmail.com>
2026-08-10 06:50:52 -0300

    update

--- web/src/lib/auth-client.ts
+++ web/src/lib/auth-client.ts
@@ -1,3 +1,10 @@
-import { createAuthClient } from 'better-auth/react'
+import { createAuthClient } from "better-auth/react"
+import { adminClient } from "better-auth/client/plugins" // Importeer de admin client plugin
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  plugins: [
+    adminClient() // Voeg de plugin hier toe
+  ]
+})
+
+export const { useSession, signIn, signOut } = authClient
