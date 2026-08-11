2346d719403b6b0f176c9b7a34588325c6b0973e
dwayneR06 <riedewalddwayne12@gmail.com>
2026-08-10 22:08:57 -0300

    Beveilig admin/users route met admin-guard

--- web/src/routes/admin/users.tsx
+++ web/src/routes/admin/users.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { requireAdminBeforeLoad } from '#/lib/require-admin-route'
+
 export const Route = createFileRoute('/admin/users')({
+  beforeLoad: requireAdminBeforeLoad,
   component: RouteComponent,
 })
 
