0e13f9380270c3590fe0b5cd35c80098bd0d80b3
dwayneR06 <riedewalddwayne12@gmail.com>
2026-08-10 22:09:13 -0300

    Beveilig admin/coompanies route met admin-guard

--- web/src/routes/admin/coompanies.tsx
+++ web/src/routes/admin/coompanies.tsx
@@ -1,10 +1,13 @@
 import CompanyTable from '#/components/companies/company-table'
 import { createFileRoute } from '@tanstack/react-router'
 
+import { requireAdminBeforeLoad } from '#/lib/require-admin-route'
+
 export const Route = createFileRoute('/admin/coompanies')({
+  beforeLoad: requireAdminBeforeLoad,
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <CompanyTable/>
+  return <CompanyTable />
 }
